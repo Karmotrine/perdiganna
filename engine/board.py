6c8d05c84f4dcb7c3e5c7ab4da25d4b6d9f18190
KirisakiXOnodera <72533293+KirisakiXOnodera@users.noreply.github.com>
2023-05-19 00:27:25 +0800

    part 3 done

--- engine/board.py
+++ engine/board.py
@@ -6,7 +6,6 @@ class Board:
     def __init__(self,):
         # 2D Representation of the Pieces
         self.board = [[]]
-        self.selected_piece = None
         self.red_left = self.black_left = 12
         self.red_kings = self.black_kings = 0
         self.create_board()
@@ -25,7 +24,7 @@ class Board:
         if row == ROWS - 1 or row == 0:
             piece.make_king()
             if piece.color == BLACK:
-                self.white_kings += 1
+                self.black_kings += 1
             else:
                 self.red_kings += 1 
 
@@ -54,3 +53,100 @@ class Board:
                 piece = self.board[row][col]
                 if piece != 0:
                     piece.draw(win)
+
+    def remove(self, pieces):
+        for piece in pieces:
+            self.board[piece.row][piece.col] = 0
+            if piece != 0:
+                if piece.color == RED:
+                    self.red_left -= 1
+                else:
+                    self.black_left -=1
+    
+    def winner(self):
+        if self.red_left <= 0:
+            return RED
+        elif self.black_left <= 0:
+            return BLACK
+
+        return None
+
+
+    def get_valid_moves(self, piece):
+        moves = {}
+        left = piece.col - 1
+        right = piece.col + 1
+        row = piece.row
+
+        if piece.color == RED or piece.king:
+            moves.update(self._traverse_left(row - 1, max(row-3, -1), -1, piece.color, left))
+            moves.update(self._traverse_right(row - 1, max(row-3, -1), -1, piece.color, right))
+        if piece.color == BLACK or piece.king:
+            moves.update(self._traverse_left(row + 1, min(row+3, ROWS), 1, piece.color, left))
+            moves.update(self._traverse_right(row + 1, min(row+3, ROWS), 1, piece.color, right))
+
+        return moves
+    
+    def _traverse_left(self, start, stop, step, color, left, skipped =[]):
+        moves = {}
+        last = []
+        for r in range(start, stop, step):
+            if left < 0:
+                break
+
+            current = self.board[r][left]
+            if current == 0:
+                if skipped and not last:
+                    break
+                elif skipped:
+                    moves[(r, left)] = last + skipped
+                else:
+                    moves[(r, left)] = last
+
+                if last:
+                    if step == -1:
+                        row = max(r-3, 0)
+                    else:
+                        row = min(r+3, ROWS)
+                    moves.update(self._traverse_left(r+step, row, step, color, left-1,skipped=last))
+                    moves.update(self._traverse_right(r+step, row, step, color, left+1,skipped=last))                    
+                break
+            elif current.color == color:
+                break
+            else:
+                last = [current]
+            left -= 1
+
+        return moves
+
+    def _traverse_right(self, start, stop, step, color, right, skipped =[]):
+        moves = {}
+        last = []
+        for r in range(start, stop, step):
+            if right >= COLS:
+                break
+
+            current = self.board[r][right]
+            if current == 0:
+                if skipped and not last:
+                    break
+                elif skipped:
+                    moves[(r, right)] = last + skipped
+                else:
+                    moves[(r, right)] = last
+
+                if last:
+                    if step == -1:
+                        row = max(r-3, 0)
+                    else:
+                        row = min(r+3, ROWS)
+                    moves.update(self._traverse_left(r+step, row, step, color, right-1,skipped=last))
+                    moves.update(self._traverse_right(r+step, row, step, color, right+1,skipped=last))                    
+                break
+            elif current.color == color:
+                break
+            else:
+                last = [current]
+            right += 1
+
+        return moves
