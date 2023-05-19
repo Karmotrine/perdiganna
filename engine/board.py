9ee8f18813a438d387ad83f5722c5a9748ac9a64
KirisakiXOnodera <72533293+KirisakiXOnodera@users.noreply.github.com>
2023-05-19 10:36:13 +0800

    double jump bug fix

--- engine/board.py
+++ engine/board.py
@@ -105,7 +105,7 @@ class Board:
 
                 if last:
                     if step == -1:
-                        row = max(r-3, 0)
+                        row = max(r-3, -1)
                     else:
                         row = min(r+3, ROWS)
                     moves.update(self._traverse_left(r+step, row, step, color, left-1,skipped=last))
@@ -137,7 +137,7 @@ class Board:
 
                 if last:
                     if step == -1:
-                        row = max(r-3, 0)
+                        row = max(r-3, -1)
                     else:
                         row = min(r+3, ROWS)
                     moves.update(self._traverse_left(r+step, row, step, color, right-1,skipped=last))
